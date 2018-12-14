da87661d4fc17b99836fa1b35756685f0045ce74
Uwe Grawert <grawert@b1-systems.de>
2018-12-14 04:23:04 +0100

    Add mkisofs command with EFI options

--- create_iso_image.sh
+++ create_iso_image.sh
@@ -1,7 +1,19 @@
-mkisofs -pad -l -r -J -v -V "Backup $HOSTNAME" -no-emul-boot -boot-load-size 4 \
- -boot-info-table \
- -b boot/isolinux/isolinux.bin \
- -c boot/isolinux/boot.cat \
- -hide-rr-moved \
+# xorriso -as mkisofs -r -V "Backup $HOSTNAME" \
+#  -hide-rr-moved \
+#  -boot-info-table \
+#  -boot-load-size 4 \
+#  -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat \
+#  -no-emul-boot \
+#  -eltorito-alt-boot -e boot/efi.img \
+#  -no-emul-boot \
+#  -o restore.iso \
+#  grml_original
+mkisofs -r -V "BACKUP" \
  -o restore.iso \
+ -c boot/isolinux/boot.cat \
+ -b boot/isolinux/isolinux.bin \
+ -no-emul-boot -boot-load-size 4 -boot-info-table \
+ -eltorito-alt-boot \
+ -eltorito-platform 0xEF -eltorito-boot boot/efi.img \
+ -no-emul-boot \
  grml_original
