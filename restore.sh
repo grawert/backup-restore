4c70b7aae93ca2f20023c8191ed003572fa20a4e
Uwe Grawert <grawert@b1-systems.de>
2018-12-16 21:49:49 +0100

    Make restore.sh script work on virtio disk

--- restore.sh
+++ restore.sh
@@ -1,55 +1,55 @@
-DISK=/dev/sda
+#!/bin/bash
+
+DISK=/dev/vda
 
 parted $DISK mklabel gpt
-parted $DISK mkpart primary  1M 3M
-parted $DISK mkpart primary fat16 3M 213M		# /boot/efi
-parted $DISK mkpart primary 513M 1024M			# /boot
-parted $DISK mkpart primary 1024M 100%			# LVM
+parted $DISK mkpart primary fat16 1M    213M	# /boot/efi
+parted $DISK mkpart primary       213M  1024M	# /boot
+parted $DISK mkpart primary       1024M 100%	# LVM
 
-parted $DISK name 1 legacy_boot
-parted $DISK name 2 UEFI
-parted $DISK name 3 lxboot
-parted $DISK name 4 primary
-parted $DISK set 1 bios_grub on
-parted $DISK set 2 boot on
+parted $DISK name 1 UEFI
+parted $DISK name 2 LXBOOT
+parted $DISK name 3 LVM
+parted $DISK set 1 boot on
 
-pvcreate "${DISK}4"
-vgcreate systemVG "${DISK}4"
+pvcreate "${DISK}3"
+vgcreate systemVG "${DISK}3"
 
 lvcreate --name LVRoot --size 5G systemVG
 lvcreate --name LVSwap --size 1G systemVG
 lvcreate --name LVvar --size 1G systemVG
 lvcreate --name LVtftp --size 1G systemVG
 
-mkfs -t vfat "${DISK}2"
-mkfs -t ext3 "${DISK}3"
+mkfs -t vfat "${DISK}1"
+mkfs -t ext3 "${DISK}2"
 mkfs -t ext4 /dev/systemVG/LVRoot
 mkfs -t ext4 /dev/systemVG/LVvar
 mkfs -t ext3 /dev/systemVG/LVtftp
 
 mount /dev/systemVG/LVRoot /mnt
 mkdir -p /mnt/boot /mnt/srv/tftpboot /mnt/var
-mount "${DISK}3" /mnt/boot
+mount "${DISK}2" /mnt/boot
 mkdir /mnt/boot/efi
-mount "${DISK}2" /mnt/boot/efi
+mount "${DISK}1" /mnt/boot/efi
 mount /dev/systemVG/LVvar /mnt/var
 mount /dev/systemVG/LVtftp /mnt/srv/tftpboot
 
-tar -xf backup.tar.gz -C /mnt
+tar --verbose --extract --file=backup.tar.gz --directory=/mnt
+
 mount -o bind /dev /mnt/dev
 mount -o bind /sys /mnt/sys
 mount -t proc /proc /mnt/proc
 
-echo > /mnt/etc/fstab <<EOF
+cat > /mnt/etc/fstab <<EOF
 devpts /dev/pts devpts mode=0620,gid=5 0 0
 proc /proc proc defautls 0 0
 
 /dev/systemVG/LVRoot / ext4 defaults 1 1
-/dev/${DISK}3 /boot ext3 defaults 1 2
-/dev/${DISK}2 /boot/efi vfat defaults 0 0
+/dev/${DISK}2 /boot ext3 defaults 1 2
+/dev/${DISK}1 /boot/efi vfat defaults 0 0
 /dev/systemVG/LVRoot / ext4 defaults 1 1
 EOF
 
 chroot /mnt grub2-mkconfig -o /boot/grub2/grub.cfg
-chroot /mnt grub2-mkconfig -o /boot/efi/EFI/BOOT/grub.cfg
+chroot /mnt grub2-mkconfig -o /boot/efi/EFI/sles/grub.cfg
 chroot /mnt grub2-install $DISK
