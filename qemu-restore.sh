9f3fd1012082325b47a41808af0cc9c02a03a702
Uwe Grawert <grawert@b1-systems.de>
2018-12-22 01:47:43 +0100

    Set block device type for qemu by file suffix

--- qemu-restore.sh
+++ qemu-restore.sh
@@ -2,10 +2,16 @@
 
 BASEDIR=$(dirname $0)
 source ${BASEDIR}/config.sh
-DISK='/var/lib/libvirt/images/restore.qcow2'
+DISK="${BACKUP_HOME}/restore.qcow2"
 
-[[ -z "$1" ]] || ISO_IMAGE=$1
-[[ -z "$2" ]] || DISK=$2
+if [[ $DISK =~ \.(.+)$ ]]; then
+    DISK_FORMAT=${BASH_REMATCH[1]}
+else
+    DISK_FORMAT="qcow2"
+fi
+
+[[ -z "$1" ]] || DISK=$1
+[[ -z "$2" ]] || ISO_IMAGE=$2
 
 qemu-system-x86_64 \
  -m 1024 \
@@ -16,5 +22,5 @@ qemu-system-x86_64 \
  -serial mon:stdio \
  -drive file=${RESTORE_ISO_FILE},format=raw,if=none,readonly=on,id=cd0 \
  -device ide-cd,bus=ide.0,unit=0,drive=cd0,id=ide0-0-0,bootindex=3 \
- -drive file=${DISK},format=qcow2,if=none,id=d0 \
+ -drive file=${DISK},format=${DISK_FORMAT},if=none,id=d0 \
  -device virtio-blk-pci,drive=d0,id=vda,bootindex=2
